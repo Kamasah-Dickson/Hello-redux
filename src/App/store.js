ef2d672d2330158940cd6cfcc5c70433f01e3c1b
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-16 04:07:58 +0000

    update redux store

--- src/App/store.js
+++ src/App/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postReducer from "../features/posts/postSlice";
-import todoReducer from "../features/Todos/todoSlice";
+// import postReducer from "../features/posts/postSlice";
+// import todoReducer from "../features/Todos/todoSlice";
+import commerceReducer from "../features/Ecommerce/commerce";
 export const store = configureStore({
 	reducer: {
-		posts: postReducer,
-		todo: todoReducer,
+		// posts: postReducer,
+		// todo: todoReducer,
+		commerce: commerceReducer,
 	},
 });
