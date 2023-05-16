7a867ea15e2a671c607553a3192348c8e2a4375c
Kamasah Dickson <kamasahdickson19@gmail.com>
2023-05-16 04:01:29 +0000

    add store Data

---
+++ src/Data.js
@@ -0,0 +1,115 @@
+import img1 from "./assets/phones/Image.svg";
+import img2 from "./assets/phones/Name=Dell-XPS-13-White.svg";
+import img3 from "./assets/phones/Name=Headphones.svg";
+import img4 from "./assets/phones/Name=Iphone-12-01.svg";
+import img5 from "./assets/phones/Name=Iphone-12-02.svg";
+import img6 from "./assets/phones/Name=Iphone-12-03.svg";
+import img7 from "./assets/phones/Name=Iphone-12-04.svg";
+import img8 from "./assets/phones/Name=Iphone-12-Pro-01.svg";
+import img9 from "./assets/phones/Name=Iphone-12-Pro-02.svg";
+import img10 from "./assets/phones/Name=Iphone-13-Pro-03.svg";
+import img11 from "./assets/phones/Name=Macbook.svg";
+import img12 from "./assets/phones/Name=Samsung-Note21.svg";
+import img13 from "./assets/phones/Name=Samsung-S21-Pro.svg";
+import img14 from "./assets/phones/Name=Samsung-S21.svg";
+
+export const commerce = [
+	{
+		image: img1,
+		id: 1345323,
+		price: 249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img2,
+		id: 23675552,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img3,
+		id: 3345345,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img4,
+		id: 42362,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img5,
+		id: 523426,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		id: 623452,
+		image: img6,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img7,
+		id: 213467,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img8,
+		id: 82352,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img9,
+		id: 92523,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img10,
+		id: 1234230,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img11,
+		id: 1627681,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img12,
+		id: 1229345,
+		price: 249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj  kjdfkj",
+	},
+	{
+		image: img13,
+		id: 123263,
+		price: 3249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj kjdfkj",
+	},
+	{
+		image: img14,
+		id: 123234,
+		price: 249.99,
+		title: "Macbook pro 16",
+		description: "lorem lfks djksj kdjfsk skjk slkj kjdfkj",
+	},
+];
